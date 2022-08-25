885a2f9662e228fc04e500b25c0dc4750cbe88aa
shechin <shechinmihail@mail.ru>
2022-08-25 08:48:50 +0500

    Сделал работу над ошибками

--- src/Main.java
+++ src/Main.java
@@ -2,69 +2,77 @@ import java.time.LocalDate;
 
 public class Main {
 
-    public static void task1_pritnyear (int yearNumber){
-        if(yearNumber % 4 == 0 && yearNumber % 100 != 0 || yearNumber % 400 == 0){
+    private static void task1PritnYear(int yearNumber) {
+        if (yearNumber % 4 == 0 && yearNumber % 100 != 0 || yearNumber % 400 == 0) {
             System.out.println(yearNumber + " високосный год");
         } else {
             System.out.println(yearNumber + " не високосный год");
         }
 
     }
-    public static void task2_definitionClientOS (int year, int versionOS){
-    int currentYear = LocalDate.now().getYear();
-    int clientDevice = versionOS;
-    int IOS = 0;
-    int Android = 1;
-    int clientDeviceYear = year;
-    if (clientDevice == Android){
-        System.out.print("На Android установите ");
-    } else if (clientDevice == IOS) {
-        System.out.print("На IOS установите ");
-    }
-    if (clientDeviceYear <currentYear) {
-        System.out.println("lite-версию");
-    } else {
-        System.out.println("обычную версию");
-    }
+
+    private static void task2DefinitionClientOS(int year, int versionOS) {
+        int currentYear = LocalDate.now().getYear();
+        int clientDevice = versionOS;
+        int ios = 0;
+        int android = 1;
+        int clientDeviceYear = year;
+        if (clientDevice == android) {
+            System.out.print("На Android установите ");
+        } else if (clientDevice == ios) {
+            System.out.print("На IOS установите ");
+        }
+        if (clientDeviceYear < currentYear) {
+            System.out.println("lite-версию");
+        } else {
+            System.out.println("обычную версию");
+        }
 
     }
-    public static void tazk3_deliverysDays (int distance){
-        int deliveryDistance = distance;
+
+    private static int task3DeliverysDays(int deliveryDistance) {
+        int distance = deliveryDistance;
         int deliveryDays = 1;
         int start = 20;
         int interval = 40;
-        if (deliveryDistance<=start) {
-            deliveryDays = 1;
+        if (deliveryDistance <= start) {
+            return 1;
         } else {
-            deliveryDays = deliveryDays + (int) Math.ceil((deliveryDistance-start)/(double) interval);
+            deliveryDays = deliveryDays + (int) Math.ceil((deliveryDistance - start) / (double) interval);
+            return deliveryDays;
         }
-        System.out.println("Потребуется дней: " + deliveryDays);
+
+
     }
 
     public static void main(String[] args) {
-    task1();
-    task2();
-    task3();
+        task1();
+        task2();
+        task3();
 
     }
-    public static void task1 () {
 
-        task1_pritnyear(2016);
-        System.out.println(" ");
-        }
+    public static void task1() {
 
-    public static void task2 (){
+        task1PritnYear(2022);
+        System.out.println();
+    }
 
-        task2_definitionClientOS(2012,0);
-        System.out.println(" ");
-        }
+    public static void task2() {
 
-    public static void task3 (){
+        task2DefinitionClientOS(2015, 0);
+        System.out.println();
+    }
 
-        tazk3_deliverysDays(25);
+    public static void task3() {
+
+        int deliveryDistance = 95;
+        int deliveryDays = task3DeliverysDays(deliveryDistance);
+        System.out.println(deliveryDays);
+
 
     }
-    }
+}
 
 
 
